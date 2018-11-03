27eed08757c21d185c42f4feff1dd4d5e36e881e
helloholodev <axensionclientsix@axensionstudios.com>
2018-11-03 15:47:52 +0800

    Binary

---
+++ src/BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch
+{
+    public static void main(String[] args)
+    {
+        int [] numsArr = {1,2,3,4,5,6,7,8,9,10};
+        int target = 3;
+        System.out.println(binarySearchF(numsArr,3));
+        
+    }
+
+
+    public static int binarySearchF(int[] nums, int target)
+    {
+        int mid = 0;
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right)
+        {
+            mid = left + (right - left)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(target<nums[mid])
+            {
+               right = mid - 1;
+            }
+            else if(target>nums[mid])
+            {
+               left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
